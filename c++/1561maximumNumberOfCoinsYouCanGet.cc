53143f4674afc1e7e2cc42963256fa99665fe6e0
acytoo <acytoo@gmail.com>
2020-08-27 20:15:12 +0800

    .

---
+++ c++/1561maximumNumberOfCoinsYouCanGet.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <climits>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <list>
+#include <unordered_set>
+#include <map>
+#include <functional>
+#include <bitset>
+#include <numeric>
+#include <deque>
+#include <mutex>
+#include <utility>
+#include <memory>
+#include <cstring>
+
+using namespace std;
+
+static int x = [] () {ios::sync_with_stdio(false); cin.tie(0); return 0;} ();
+class Solution {
+ public:
+  int maxCoins(vector<int>& piles) {
+    sort(piles.rbegin(), piles.rend());
+    int res = 0;
+    for (int i = 1, stop = piles.size() / 3 * 2; i < stop; i += 2)
+      res += piles[i];
+    return res;
+  }
+};
+
+int main() {
+  Solution s;
+  
+  return 0;
+}
