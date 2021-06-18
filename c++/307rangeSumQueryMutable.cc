84e67b4a18599bc36ec2ceca2709d9f520196a81
acytoo <acytoo@gmail.com>
2021-06-18 11:26:47 -0400

    .

--- c++/307rangeSumQueryMutable.cc
+++ c++/307rangeSumQueryMutable.cc
@@ -25,6 +25,46 @@ static int x = [] () {ios::sync_with_stdio(false); cin.tie(0); return 0;} ();
 class NumArray {
  public:
   NumArray(vector<int>& nums) {
+    if (nums.empty()) return ;
+    const int data_size = nums.size();
+    len_ = ceil(sqrt(data_size));  // also number of blocks
+    block_ = vector<int> (len_);
+    for (int i = 0; i < data_size; ++i)
+      block_[i / len_] += nums[i];
+    data_ = nums;
+  }
+
+  void update(int index, int val) {
+    const int block_idx = index / len_;
+    block_[block_idx] += val - data_[index];
+    data_[index] = val;
+  }
+
+  int sumRange(int left, int right) {
+    int res = 0;
+    int start = left / len_, end = right / len_;
+    if (start == end) {
+      for (int i = left; i <= right; ++i)
+        res += data_[i];
+      return res;
+    }
+    for (int i = left, stop = (start + 1) * len_; i < stop; ++i)
+      res += data_[i];
+    for (int i = start + 1; i < end; ++i)
+      res += block_[i];
+    for (int i = end * len_; i <= right; ++i)
+      res += data_[i];
+    return res;
+  }
+ private:
+  vector<int> data_, block_;
+  int len_;
+};
+
+
+class NumArray_TLE {  // AC at 04/19/2020 08:13 and TLE at 06/18/2021
+ public:
+  NumArray(vector<int>& nums) {
     const int n = nums.size();
     if (n == 0) return ;
     nums_ = nums;
