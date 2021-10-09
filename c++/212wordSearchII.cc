7a9f26b76df6b3db922b537ba21b7381a16edf92
acytoo <acytoo@gmail.com>
2021-10-08 23:43:40 -0400

    .

--- c++/212wordSearchII.cc
+++ c++/212wordSearchII.cc
@@ -24,6 +24,53 @@ static int x = [] () {ios::sync_with_stdio(false); cin.tie(0); return 0;} ();
 class Solution {
  public:
   vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    const int m = board.size(), n = board[0].size();
+    vector<string> res;
+    set<char> st;
+    for (const string &word : words) st.insert(word.back());
+    constexpr int dirs[] = {0, -1, 0, 1, 0};
+
+    function<bool(string_view, int, int, int)> dfs =
+        [&] (const string_view word, const int x, const int y, const int idx) {
+          if (idx == -1) return true;
+          if (x < 0 || x >= m || y < 0 || y >= n) return false;
+          if (word[idx] != board[x][y]) return false;
+          const char c = board[x][y];
+          board[x][y] = '-';
+          for (int i = 0; i < 4; ) {
+            if (dfs(word, x + dirs[i], y + dirs[++i], idx - 1)) {
+              board[x][y] = c;
+              return true;
+            }
+          }
+          board[x][y] = c;
+          return false;
+        };
+
+    map<char, vector<pair<int, int>>> mp;  // char -> (row, col)
+    for(int i = 0; i < m; ++i) {
+      for(int j = 0; j < n; ++j) {
+        const char c = board[i][j];
+        if (st.find(c) != st.end())
+          mp[c].push_back({i, j});
+      }
+    }
+    for (const string &word : words) {
+      const char c = word.back();
+      for (const auto &[x, y] : mp[c]) {
+        if (dfs(word, x, y, word.size() - 1)) {
+          res.push_back(word);
+          break;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+class Solution_TLS {
+ public:
+  vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
     row_ = board.size();
     if (row_ == 0)
       return {};
@@ -54,9 +101,9 @@ class Solution {
     char cur = board[x][y];  // use a mask '#'
     board[x][y] = '#';
     bool res = found(board, word, idx+1, x+1, y) ||
-          found(board, word, idx+1, x-1, y) ||
-          found(board, word, idx+1, x, y+1) ||
-          found(board, word, idx+1, x, y-1);
+               found(board, word, idx+1, x-1, y) ||
+               found(board, word, idx+1, x, y+1) ||
+               found(board, word, idx+1, x, y-1);
     board[x][y] = cur;
     return res;
   }
