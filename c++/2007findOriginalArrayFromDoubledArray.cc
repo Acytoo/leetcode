7752bd4872db6e0de3d8cb9e9ff9ee1d6ab58201
acytoo <acytoo@gmail.com>
2021-11-06 17:08:36 -0400

    .

--- c++/2007findOriginalArrayFromDoubledArray.cc
+++ c++/2007findOriginalArrayFromDoubledArray.cc
@@ -24,7 +24,38 @@
 using namespace std;
 
 static int x = [] () {ios::sync_with_stdio(false); cin.tie(0); return 0;} ();
-
+class Solution {
+ public:
+  vector<int> findOriginalArray(vector<int>& changed) {
+    const int n = changed.size();
+    if (n & 1) return {};
+    int nums[100001];
+    memset(nums, 0, sizeof nums);
+    int maxnumber = -1;
+    for (const int a : changed) {
+      maxnumber = max(maxnumber, a);
+      ++nums[a];
+    }
+    int idx = 0, i = maxnumber, stop = n >> 1;
+    vector<int> res;
+    while (idx < stop) {
+      if (nums[i] < 0) return {};
+      if (nums[i] == 0) {
+        --i;
+        continue;
+      }
+      if (i & 1) return {};
+      const int ori = i >> 1;
+      res.emplace_back(ori);
+      --nums[ori];
+      --nums[i];
+      ++idx;
+    }
+    for (int i = 0; i < maxnumber; ++i)
+      if (nums[i] != 0) return {};
+    return res;
+  }
+};
 
 int main() {
   Solution s;
